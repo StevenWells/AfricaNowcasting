4009f755163f52bfbb4e897acfbeaff8d6ea4f6b
StevenWells <stewells@ceh.ac.uk>
2026-05-27 12:53:01 +0100

    Updated netNCC shell script to accomodate change in pyton venv name

--- run_netncc_nowcasts.sh
+++ run_netncc_nowcasts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 date
 source /etc/profile.d/conda.sh
-conda activate nflics_netncc
+conda activate py311_netncc
 
 python /home/stewells/AfricaNowcasting/rt_code/PanAfrica_NetNCC_leadtimes_1to6_0p05deg.py --mode realtime
 
