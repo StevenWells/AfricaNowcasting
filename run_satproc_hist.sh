5fdb2539db4677e1d06f628100b0cc035c3e4ca5
StevenWells <stewells@ceh.ac.uk>
2026-07-20 11:20:03 +0100

    Minor edit to historical run nflics shell script

--- run_satproc_hist.sh
+++ run_satproc_hist.sh
@@ -2,5 +2,5 @@
 date
 source /etc/profile.d/conda.sh
 conda activate py37
-python /home/stewells/AfricaNowcasting/rt_code/sat_transfer.py historical --startDate 202012010000 --endDate 202012010000
+python /home/stewells/AfricaNowcasting/rt_code/sat_transfer.py historical --fStruct YMD --startDate 202606011200 --endDate 202606011200
 date
